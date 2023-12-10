397ba5034fe279be437811a0493f03b4f090f44a
Oleksandr Shyian <shyian.code@gmail.com>
2023-12-10 21:41:30 +0200

    TypeScript OOP

--- src/1.ts
+++ src/1.ts
@@ -1,15 +1,11 @@
 /*
   Клас Student, який містить три властивості: name, age та grade. 
-  Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, 
-  напишіть скорочену ініціалізацію.
+  Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, 
+  і нарешті надавати їм значення, напишіть скорочену ініціалізацію.
 */
 
 class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
+  constructor(public name: string, public age: number, public grade: string) {
     this.name = name;
     this.age = age;
     this.grade = grade;
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,67 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+// Клас Key
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature() {
+      return this.signature;
+    }
+  }
+  
+  // Клас Person
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey() {
+      return this.key;
+    }
+  }
+  
+  // Абстрактний клас House
+  abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person) {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log('Person entered the house.');
+      } else {
+        console.log('The door is closed. Person cannot enter.');
+      }
+    }
+  }
+  
+  // Клас MyHouse, успадкований від House
+  class MyHouse extends House {
+    openDoor(key: Key) {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('The door is open.');
+      } else {
+        console.log('Invalid key. The door remains closed.');
+      }
+    }
+  }
+  
+  // Використання класів
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);  
